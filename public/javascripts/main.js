93a7f44d85fa978bcb43e95077e38e3d6a7ccc27
CaptainJH <heqiju@gmail.com>
2015-09-09 00:19:42 +0800

    draw streamgraph

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,54 +1,60 @@
 
 function createSoccerViz() {
-	d3.csv('/data/worldcup.csv', function(data){
+	d3.csv('/data/movies.csv', function(data){
 		console.log(data);
 		overallTeamViz(data);
-	})
+	});
 }
 
 function overallTeamViz(incomingData) {
 	
-	var scatterData = [{friends: 5, salary: 22000},
-		{friends: 3, salary: 18000}, {friends: 10, salary: 88000},
-		{friends: 0, salary: 180000}, {friends: 27, salary: 56000},
-		{friends: 8, salary: 74000}];
-		
-		 
-	var xExtent = d3.extent(scatterData, function(d) {
-                 return d.salary;
-              });
-	var yExtent = d3.extent(scatterData, function(d) {
-                 return d.friends;
-              });
+	var fillScale = d3.scale.linear()
+            .domain([0,5])
+            .range(["lightgray","black"]);
+	var n = 0;
 			  
-	var xScale = d3.scale.linear().domain(xExtent).range([0,500]);
-	var yScale = d3.scale.linear().domain(yExtent).range([0,500]);
-	d3.select("svg").selectAll("circle")
-			.data(scatterData).enter().append("circle")
-			.attr("r", 5).attr("cx", function(d) {
-					return xScale(d.salary);
-			}).attr("cy", function(d) {
-					return yScale(d.friends);
-					});
-					
-	var yAxis = d3.svg.axis().scale(yScale).orient("right");
-	d3.select("svg").append("g").attr("id", "yAxisG").call(yAxis);
-	var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
-	d3.select("svg").append("g").attr("id", "xAxisG").call(xAxis);
-
-
-	var lineVar = d3.svg.line()
-		.x(function(d) {
-			return xScale(d.salary);
-		})
-		.y(function(d) {
-		return yScale(d.friends);
-		});
+	var xScale = d3.scale.linear().domain([ 1, 8 ]).range([ 20, 470 ]);
+	var yScale = d3.scale.linear().domain([ 0, 100 ]).range([ 480, 20 ]);	
+	
+	for(x in incomingData[0])
+	{
+		if( x != "day")
+		{
+			var movieArea = d3.svg.area().x(function(d){
+				return xScale(d.day);
+			}).y(function(d){
+				return yScale(simpleStacking(d, x))
+			}).y0(function(d){
+				return yScale(simpleStacking(d, x) - d[x]);
+			}).interpolate("basis");
+			
+			d3.select("svg")
+				.append("path")
+				.style("id", x + "Area")
+				.attr("d", movieArea(incomingData))
+				.attr("fill", "darkgray")
+				.attr("stroke", "lightgray")
+				.attr("stroke-width", 2)
+				.style("opacity", .5);
+				
+		}
+	}
+	
+	function simpleStacking(incomingData, incomingAttribute)
+	{
+		var newHeight = 0;
 		
-	d3.select("svg")
-		.append("path")
-		.attr("d", lineVar(scatterData))
-		.attr("fill", "none")
-		.attr("stroke", "darkred")
-		.attr("stroke-width", 2);
+		for(x in incomingData)
+		{
+			if(x != "day")
+			{
+				newHeight += parseInt(incomingData[x]);
+				if(x == incomingAttribute)
+				{
+					break;
+				}
+			}
+		}
+		return newHeight;
+	}
  }
