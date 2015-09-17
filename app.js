7aeca37cc32497a4e22ca93e81125e3d05840d1c
CaptainJH <heqiju@gmail.com>
2015-09-18 00:32:18 +0800

    add function to get server IP address

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var os = require('os');
+
 var app = express();
 
 // view engine setup
@@ -94,3 +96,33 @@ responder.bind('tcp://*:6292', function(err) {
     console.log("Listening on 6292…");
   }
 });
+
+
+function GetSelfIPAddress() {
+  var ifaces = os.networkInterfaces();
+  
+  var address = 0;
+
+  Object.keys(ifaces).forEach(function (ifname) {
+    var alias = 0;
+  
+    ifaces[ifname].forEach(function (iface) {
+      if ('IPv4' !== iface.family || iface.internal !== false) {
+        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+        return;
+      }
+  
+      if (alias >= 1) {
+        // this single interface has multiple ipv4 addresses
+        console.log(ifname + ':' + alias, iface.address);
+      } else {
+        // this interface has only one ipv4 adress
+        console.log(ifname, iface.address);
+      }
+      
+      address = iface.address; 
+    });
+  });
+  
+  return address;
+}
